7fe5a62cc5dbe2ae848abfd1fc9efb3d16fe8b9a
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2024-02-14 22:08:42 +0300

    0.0
    Start

---
+++ tetris-game/src/components/Game.js
@@ -0,0 +1,15 @@
+import Menu from "./Menu";
+//-------------------------------------
+const Game = ({ rows, columns }) => {
+  const start = () => {
+    console.log("Start");
+  };
+
+  return (
+    <div className="Game">
+      <Menu onClick={start} />
+    </div>
+  );
+};
+//-------------------------------------
+export default Game;
